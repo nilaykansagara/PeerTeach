29d48980fa380116be06ae7e63c0e95417493e94
Nilay Kansagara <nilaykansagara@gmail.com>
2024-03-06 00:43:42 +0530

    Slider

--- client/src/Purchaseform.jsx
+++ client/src/Purchaseform.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import { useState, useEffect } from 'react';
 import { useDisclosure } from '@chakra-ui/react';
 import { Input } from '@chakra-ui/react';
 import { FormControl, FormLabel } from '@chakra-ui/react';
@@ -6,6 +8,8 @@ import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody,
 import { Button } from '@chakra-ui/react';
 import { useNavigate } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import {
     Drawer,
     DrawerBody,
@@ -15,6 +19,7 @@ import {
     DrawerContent,
     DrawerCloseButton,
 } from '@chakra-ui/react'
+import { faTimeline, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 
 
@@ -23,12 +28,15 @@ function Purchaseform() {
     let sctemp = localStorage.getItem('slclStudents');
     const selectedCollege = JSON.parse(ctemp);
     const slclStudents = JSON.parse(sctemp);
+    const [flag, setFlag] = useState(0);
     const { isOpen, onOpen, onClose } = useDisclosure();
+
     const btnRef = React.useRef();
     const navigate = useNavigate();
     const homeClicked = () => {
         navigate('/BusinessmanHome');
     }
+
 
     return (
         <>
@@ -54,7 +62,7 @@ function Purchaseform() {
                                     <a className="nav-link active" style={{ marginTop: '5px', color: '#C8C8C8', fontSize: '17px', cursor: 'pointer' }} aria-current="page" onClick={homeClicked}><b>Home</b></a>
                                 </li>
                                 <li className="nav-item">
-                                    <a className="nav-link active" style={{ marginTop: '5px', color: '#C8C8C8', fontSize: '17px', cursor: 'pointer' }} aria-current="page" onClick={onOpen}><b>Help</b></a>
+                                    <a className="nav-link active" style={{ marginTop: '5px', color: '#C8C8C8', fontSize: '17px', cursor: 'pointer' }} aria-current="page" onClick={onOpen} isOpen={isOpen}><b>Help</b></a>
                                 </li>
 
                             </ul>
@@ -63,94 +71,129 @@ function Purchaseform() {
                 </nav>
             </div>
 
-            <div style={{ marginLeft: '1.5%' }}>
-                <p>
-                    You select <b>{selectedCollege.name}</b> to give advertisement which have total <b>{slclStudents}</b> registered student on Peerteach. Fill the below form to generate invoice for cost that you need to pay for advertisement
-                </p>
-                <p>
-                    for cost and other for other plan please click on <b>Help</b>.
-                </p>
-                <p>
-                    <b>Note: </b>here add will be run only for a day and if there is exam time add will be run bases on hour with different cost.
-                </p>
-            </div>
-            <div style={{ display: 'flex' }}>
-                <div style={{ marginLeft: '1.3%', width: '30%', border: '2px solid black', borderRadius: '30px', backgroundColor: '#d9f0f4' }}>
-                    <FormControl style={{ padding: '10% 15% 7% 15%' }}>
-                        <div style={{ marginBottom: '9%' }}>
-                            <FormLabel><b>Number of Students per Semester</b></FormLabel>
-                            <Input type='number' placeholder='between 1 and 3' />
-                        </div>
-                        <div style={{ marginBottom: '9%' }}>
-                            <FormLabel><b>Upload Image or Video</b></FormLabel>
-                            <Input type='file' accept='image/*, video/*' />
-                        </div>
-                        <div style={{ marginBottom: '9%' }}>
-                            <FormLabel><b>Select Duration (in seconds)</b></FormLabel>
-                            <select id="durationSelect" name="durationSelect">
-                                <option value="4">4 seconds</option>
-                                <option value="5">5 seconds</option>
-                                <option value="10">10 seconds</option>
-                                <option value="20">20 seconds</option>
-                                <option value="30">30 seconds</option>
-                                <option value="60">60 seconds</option>
-                            </select>
-                        </div>
-                        <div className="ci">
-                            <button className='invoice-button' variant='ghost'>
-                                Create Invoice
-                            </button>
-                        </div>
-                    </FormControl>
-                </div>
 
 
-                {/* <div style={{ width: '25%', border: '2px solid black', borderRadius: '90px 7px 90px 7px', backgroundColor: '#ffad99', marginLeft: 'auto', marginRight: '1.3%' }}>
-                    <h3 style={{ textAlign: 'center' }}>Information</h3>
-                </div> */}
-                <Drawer
-                    isOpen={isOpen}
-                    placement='right'
-                    onClose={onClose}
-                    finalFocusRef={btnRef}
-                >
-                    <DrawerOverlay />
-                    <DrawerContent
-                        style={{
-                            marginTop: '7%',
-                            maxWidth: '40%',
-                            maxHeight: '70vh', // Set maximum height
-                            backgroundColor: '#e9e9e9',
-                            marginRight: '1.3%',
-                            border: '2px solid black',
-                            borderRadius: '40px 0px 40px 0px',
-                            overflowY: 'auto' // Enable vertical scrolling
-                        }}
-                    >
-                        <DrawerBody style={{ textAlign: 'center' }}>
-                            <div style={{ padding: '5%' }}>
-                                {slclStudents}
-                                <br />
-                                {selectedCollege.name}
-                                <br />
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id. Semper quis lectus nulla at volutpat diam ut venenatis. Dolor morbi non arcu risus quis varius quam quisque. Massa ultricies mi quis hendrerit dolor magna eget est lorem. Erat imperdiet sed euismod nisi porta. Lectus vestibulum mattis ullamcorper velit.
-                            </div>
-                            <Button
-                                variant='outline'
-                                position='absolute'
-                                bottom='4%'
-                                right='4%'
-                                onClick={onClose}
+
+
+            <Carousel style={{ display: 'flex', backgroundColor: '#c9c0bb ', height: '475px', width: '97.50%', marginLeft: '1.3%' }}
+                prevIcon={<span className="carousel-control-prev-icon" />}
+                nextIcon={<span className="carousel-control-next-icon" />}
+            >
+                <Carousel.Item>
+                    <div style={{ marginLeft: '1.5%', color: 'black', margin: '1% 7% 1% 7%', fontSize: '20px' }}>
+                        <p>
+                            You select <b>{selectedCollege.name}</b> to give advertisement which have total <b>{slclStudents}</b> registered student on Peerteach. Fill the below form to generate invoice for cost that you need to pay for advertisement
+                        </p>
+                        <p>
+                            for cost and other for other plan please click on <b>Help</b>.
+                        </p>
+                        <p>
+                            <b>Note: </b>here add will be run only for a day and if there is exam time add will be run bases on hour with different cost.
+                        </p>
+                    </div>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <div style={{ marginLeft: '1.5%', color: 'black', margin: '1% 7% 1% 7%' }}>
+                        <p>
+                            You select <b>{selectedCollege.name}</b> to give advertisement which have total <b>{slclStudents}</b> registered student on Peerteach. Fill the below form to generate invoice for cost that you need to pay for advertisement
+                        </p>
+                        <p>
+                            for cost and other for other plan please click on <b>Help</b>.
+                        </p>
+                        <p>
+                            <b>Note: </b>here add will be run only for a day and if there is exam time add will be run bases on hour with different cost.
+                        </p>
+                    </div>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <div style={{ marginLeft: '1.5%', color: 'black', margin: '1% 7% 1% 7%' }}>
+                        <p>
+                            You select <b>{selectedCollege.name}</b> to give advertisement which have total <b>{slclStudents}</b> registered student on Peerteach. Fill the below form to generate invoice for cost that you need to pay for advertisement
+                        </p>
+                        <p>
+                            for cost and other for other plan please click on <b>Help</b>.
+                        </p>
+                        <p>
+                            <b>Note: </b>here add will be run only for a day and if there is exam time add will be run bases on hour with different cost.
+                        </p>
+                    </div>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <div >
+                        <div style={{ marginLeft: '5%', width: '35%', height: '350px', border: '2px solid black', borderRadius: '30px', backgroundColor: '#d9f0f4' }}>
+                            <FormControl style={{ padding: '4% 8% 4% 8%' }}>
+                                <div style={{ marginBottom: '9%' }}>
+                                    <p><b>Number of Students per Semester</b></p>
+                                    <Input type='number' placeholder='between 1 and 3' />
+                                </div>
+                                <div style={{ marginBottom: '9%' }}>
+                                    <FormLabel><b>Upload Image or Video</b></FormLabel>
+                                    <Input type='file' accept='image/*, video/*' />
+                                </div>
+                                <div style={{ marginBottom: '9%' }}>
+                                    <FormLabel><b>Select Duration (in seconds)</b></FormLabel>
+                                    <select id="durationSelect" name="durationSelect">
+                                        <option value="4">4 seconds</option>
+                                        <option value="5">5 seconds</option>
+                                        <option value="10">10 seconds</option>
+                                        <option value="20">20 seconds</option>
+                                        <option value="30">30 seconds</option>
+                                        <option value="60">60 seconds</option>
+                                    </select>
+                                </div>
+                                <div className="ci">
+                                    <button className='invoice-button' variant='ghost'>
+                                        Create Invoice
+                                    </button>
+                                </div>
+                            </FormControl>
+                        </div>
+                        <Drawer
+                            isOpen={isOpen}
+                            placement='right'
+                            onClose={onClose}
+                            finalFocusRef={btnRef}
+                        >
+                            <DrawerOverlay />
+                            <DrawerContent
+                                style={{
+                                    marginTop: '9%',
+                                    maxWidth: '40%',
+                                    maxHeight: '70vh', // Set maximum height
+                                    backgroundColor: '#e9e9e9',
+                                    marginRight: '7%',
+                                    border: '2px solid black',
+                                    borderRadius: '40px 0px 40px 0px',
+                                    overflowY: 'auto' // Enable vertical scrolling
+                                }}
                             >
-                                Cancel
-                            </Button>
-                        </DrawerBody>
-                    </DrawerContent>
-                </Drawer>
+                                <DrawerBody style={{ textAlign: 'center' }}>
+                                    <div style={{
+                                        marginLeft: '93%',
+                                        marginTop: '2%',
+                                        cursor: 'pointer'
+                                    }}
+
+                                        onClick={onClose}
+                                    >
+                                        <FontAwesomeIcon icon={faTimes} />
+                                    </div>
+                                    <div style={{ padding: '5%' }}>
+                                        {slclStudents}
+                                        <br />
+                                        {selectedCollege.name}
+                                        <br />
+                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Consequat nisl vel pretium lectus quam id. Semper quis lectus nulla at volutpat diam ut venenatis. Dolor morbi non arcu risus quis varius quam quisque. Massa ultricies mi quis hendrerit dolor magna eget est lorem. Erat imperdiet sed euismod nisi porta. Lectus vestibulum mattis ullamcorper velit.
+                                    </div>
+
+                                </DrawerBody>
+                            </DrawerContent>
+                        </Drawer>
+                    </div >
+                </Carousel.Item>
+            </Carousel>
 
 
-
-            </div >
         </>
     );
 }
